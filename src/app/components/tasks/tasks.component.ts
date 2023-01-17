5519d51bfb15243be101426d75368c2b55f844a5
Miri <mirifurman@gmail.com>
2023-01-17 22:24:29 +0200

    toggle reminder (update)

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -5,19 +5,27 @@ import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-
 export class TasksComponent {
-tasks:Task[]=[];
+  tasks: Task[] = [];
 
-constructor(private taskService:TaskService){}
+  constructor(private taskService: TaskService) {}
 
-ngOnInit():void{
-this.taskService.getTasks().subscribe((tasks)=>this.tasks=tasks);
-}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 
-deleteTask(task:Task){
-  this.taskService.deleteTask(task).subscribe(()=>this.tasks=this.tasks.filter(t=>t.id !==task.id));
-}
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  toggleReminder(task:Task){
+    task.reminder=!task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
 }
