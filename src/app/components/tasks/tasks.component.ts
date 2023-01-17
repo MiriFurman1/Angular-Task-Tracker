be6424b726b56b6f91a3ebd2f143a4dacb347321
Miri <mirifurman@gmail.com>
2023-01-17 14:42:50 +0200

    task service added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { TASKS } from 'src/app/mock-task';
 import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
+
 export class TasksComponent {
-tasks:Task[]=TASKS;
+tasks:Task[]=[];
+
+constructor(private taskService:TaskService){}
+
+ngOnInit():void{
+ this.tasks= this.taskService.getTasks()
+}
 }
